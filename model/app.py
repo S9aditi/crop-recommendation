d2abac7e45306d913411cfd24ff55863b1264994
S9aditi <88798630+S9aditi@users.noreply.github.com>
2023-04-16 01:20:22 +0530

    Add files via upload

---
+++ model/app.py
@@ -0,0 +1,44 @@
+from flask import Flask,render_template,request, redirect,url_for
+import pickle
+import numpy as np
+from scipy.special import expit
+
+model = pickle.load(open('model.pkl', 'rb'))
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/login')
+def login():
+    return render_template('main.html')
+
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    check = False
+    if request.method=='POST':
+        check=True
+        int_features = [float(x) for x in request.form.values()]
+        
+        final_features = [np.array(int_features)]
+        prediction = model.predict(final_features)
+        output = str(prediction[0])
+        
+        return render_template("main.html",check=check,prediction_text=output)
+        
+    elif request.method == 'GET':
+        return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
